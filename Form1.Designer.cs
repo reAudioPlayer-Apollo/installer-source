8b1b79b86c11a5d81802eb1c794d06194ea934c3
dxstiny <dxstiny@users.noreply.github.com>
2021-04-28 20:37:16 +0200

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Updater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnInstall = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnInstall
+            // 
+            this.btnInstall.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInstall.Font = new System.Drawing.Font("Segoe UI Semilight", 9F);
+            this.btnInstall.Location = new System.Drawing.Point(113, 235);
+            this.btnInstall.Name = "btnInstall";
+            this.btnInstall.Size = new System.Drawing.Size(387, 34);
+            this.btnInstall.TabIndex = 0;
+            this.btnInstall.Text = "Install";
+            this.btnInstall.UseVisualStyleBackColor = true;
+            this.btnInstall.Click += new System.EventHandler(this.btnInstall_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semilight", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(219, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(281, 40);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "reAudioPlayer Apollo";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Segoe UI Light", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.ForeColor = System.Drawing.Color.White;
+            this.lblStatus.Location = new System.Drawing.Point(222, 186);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(56, 20);
+            this.lblStatus.TabIndex = 2;
+            this.lblStatus.Text = "Installer";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(113, 119);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Firebrick;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(593, -4);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(20, 26);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "x";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.ClientSize = new System.Drawing.Size(614, 383);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnInstall);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "reAudioPlayer Apollo - Installer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInstall;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using LibGit2Sharp;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Updater
+{
+    public partial class Form1 : Form
+    {
+        string githubUrl = @"https://github.com/reAudioPlayer-Apollo/release";
+        string updaterUrl = @"https://github.com/reAudioPlayer-Apollo/installer";
+        string repoLocation = "reAudioPlayer Apollo/Player";
+        string updaterLocation = "reAudioPlayer Apollo/Updater";
+
+        string pullPath = "../Player";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            var loc = AppDomain.CurrentDomain.BaseDirectory;
+            var parent = new DirectoryInfo(loc).Parent.Name;
+
+            if (parent == "reAudioPlayer Apollo")
+            {
+                //MessageBox.Show("updating!");
+                update();
+            }
+        }
+
+        private void btnInstall_Click(object sender, EventArgs e) // clone
+        {
+            Task.Factory.StartNew(() => install());
+        }
+
+        void install()
+        {
+            lblStatus.Invoke(new Action(() =>
+            {
+                lblStatus.Text = "Downloading player (1 / 2)...";
+            }));
+
+            Repository.Clone(githubUrl, repoLocation);
+
+            lblStatus.Invoke(new Action(() =>
+            {
+                lblStatus.Text = "Downloading updater (2 / 2)...";
+            }));
+
+            Repository.Clone(updaterUrl, updaterLocation);
+
+            lblStatus.Invoke(new Action(() =>
+            {
+                lblStatus.Text = "Finished download & installation!";
+            }));
+
+            btnInstall.Invoke(new Action(() =>
+            {
+                btnInstall.Text = "Finish";
+                btnInstall.Click -= btnInstall_Click;
+                btnInstall.Click += btnExit_Click;
+            }));
+        }
+
+        private void update() // pull
+        {
+            using (var repo = new Repository(pullPath))
+            {
+                var sig = new Signature("test", "test@test.com", new DateTimeOffset(DateTime.Now));
+                var opt = new PullOptions();
+                opt.MergeOptions = new MergeOptions();
+                opt.MergeOptions.FileConflictStrategy = CheckoutFileConflictStrategy.Theirs;
+                opt.MergeOptions.MergeFileFavor = MergeFileFavor.Theirs;
+
+                repo.Stashes.Add(sig, "Stash on master");
+
+                Commands.Pull(repo, sig, opt);
+                /*
+                var remote = repo.Network.Remotes["origin"];
+                var refSpecs = remote.FetchRefSpecs.Select(x => x.Specification);
+                Commands.Fetch(repo, remote.Name, refSpecs, null, logMessage);*/
+
+                //repo.Stashes.Apply(0);
+            }
+            //MessageBox.Show("successfully updated!");
+            Environment.Exit(0);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(-1);
+        }
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void Form1_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+    }
+}
